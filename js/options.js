c33eb56f923e6783c2544b0368711a49d9def30f
topcodewizard007 <mykytaya007@gmail.com>
2022-06-10 20:36:42 +0300

    upgrade into MV3 and fixed issues

--- js/options.js
+++ js/options.js
@@ -1,25 +1,29 @@
 // Extension by BrowserNative apps@browsernative.com https://browsernative.com/click-and-clean-chrome-extension/
+const LS = chrome.storage.local;
 
-$(window).on('load', function() {
-    let whitelist = localStorage.getItem("whitelist");
-    if(whitelist) whitelist = whitelist.split(",");
-    else whitelist = [];
-    for(i=0; i<whitelist.length; i++){
-        document.getElementById(whitelist[i]).checked = false;
-    }
+$(window).on("load", function () {
+  let whitelist = LS.get("whitelist", function(result) {
+    return result.whitelist;
+  });
+  if (whitelist) whitelist = whitelist.split(",");
+  else whitelist = [];
+  for (i = 0; i < whitelist.length; i++) {
+    document.getElementById(whitelist[i]).checked = false;
+  }
 });
 
-whitelist = localStorage.getItem("whitelist");
-if(whitelist) whitelist = whitelist.split(",");
+whitelist = LS.get("whitelist", function(result) {
+  return result.whitelist;
+});
+if (whitelist) whitelist = whitelist.split(",");
 else whitelist = [];
 
-$(document).on('change', "input", function() {
-    if(!this.checked) {
-        whitelist.push(this.id);
-    }
-    else if(this.checked) {
-        var index = whitelist.indexOf(this.id);
-        if (index > -1) whitelist.splice(index, 1);
-    }
-    localStorage.setItem("whitelist", whitelist);
+$(document).on("change", "input", function () {
+  if (!this.checked) {
+    whitelist.push(this.id);
+  } else if (this.checked) {
+    var index = whitelist.indexOf(this.id);
+    if (index > -1) whitelist.splice(index, 1);
+  }
+  LS.set({ "whitelist": whitelist });
 });
